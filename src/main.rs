e23e03bbf5146971bd44c2e56178d014426b60d5
desbma <desbma@users.noreply.github.com>
2021-04-09 21:17:15 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // https://www.insee.fr/fr/statistiques/2540004?sommaire=4767262
+    // TODO download https://www.insee.fr/fr/statistiques/fichier/2540004/nat2019_csv.zip
+    println!("Hello, world!");
+}
