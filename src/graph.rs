71c3e1754324331ec5dcc9211824b1983da8b616
desbma <desbma@users.noreply.github.com>
2021-04-20 01:11:14 +0200

    Simplify graph data structure

--- src/graph.rs
+++ src/graph.rs
@@ -1,51 +1,51 @@
 use indicatif::ProgressIterator;
-use std::collections::HashMap;
 
 pub type Distance = f32;
 pub type Index = u16;
 
 pub struct NameGraph {
-    distances: HashMap<(Index, Index), Distance>, // TODO use 2 hashmaps or BTreeMap instead
+    distances: Vec<Vec<Distance>>,
     name_count: usize,
 }
 
 impl NameGraph {
     pub fn new(len: usize) -> NameGraph {
         NameGraph {
-            distances: HashMap::new(),
+            distances: Vec::new(),
             name_count: len,
         }
     }
 
     pub fn fill(&mut self, names: &[String]) {
         // Init progressbar
-        let dist_count = IndexIterator::new(self.name_count).len();
-        debug_assert_eq!(dist_count, IndexIterator::new(self.name_count).count());
-        let progress = indicatif::ProgressBar::new(dist_count as u64);
+        let progress = indicatif::ProgressBar::new(self.name_count as u64);
         progress.set_style(
             indicatif::ProgressStyle::default_bar()
                 .template("Computing Levenshtein distances {percent}% {wide_bar} [🕒 {elapsed_precise} ETA {eta_precise}]"),
         );
-        progress.set_draw_delta(dist_count as u64 / 100);
+        progress.set_draw_delta(self.name_count as u64 / 100);
 
         // Compute distances
-        self.distances.reserve(dist_count);
-        self.distances.extend(
-            IndexIterator::new(self.name_count)
-                .progress_with(progress)
-                .map(|(i, j)| {
-                    (
-                        (i as Index, j as Index),
-                        Self::compute_distance(i, j, &names),
-                    )
-                }),
-        );
+        self.distances.reserve(self.name_count - 1);
+        for i in (0..self.name_count - 1).progress_with(progress) {
+            let mut cur_vec: Vec<Distance> = Vec::new();
+            cur_vec.reserve(self.name_count - i - 1);
+            for j in i + 1..self.name_count {
+                cur_vec.push(Self::compute_distance(i, j, names));
+            }
+            self.distances.push(cur_vec);
+        }
     }
 
     #[allow(dead_code)]
     pub fn get_distance(&self, a: Index, b: Index) -> Distance {
-        let key = if a <= b { (a, b) } else { (b, a) };
-        *self.distances.get(&key).unwrap()
+        if a == b {
+            0.0
+        } else if a <= b {
+            self.distances[b as usize][b as usize - a as usize - 1]
+        } else {
+            self.distances[a as usize][a as usize - b as usize - 1]
+        }
     }
 
     fn compute_distance(a: usize, b: usize, names: &[String]) -> Distance {
@@ -54,47 +54,5 @@ impl NameGraph {
         let dist = strsim::normalized_levenshtein(name_a, name_b);
         log::trace!("#{} {:?} - #{} {:?} = {}", a, name_a, b, name_b, dist);
         dist as f32
-    }
-}
-
-#[derive(Clone)]
-struct IndexIterator {
-    i: usize,
-    j: usize,
-    len: usize,
-}
-
-impl IndexIterator {
-    pub fn new(len: usize) -> IndexIterator {
-        IndexIterator { i: 0, j: 1, len }
-    }
-}
-
-impl Iterator for IndexIterator {
-    type Item = (usize, usize);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while self.i < self.len - 1 {
-            if self.j < self.len {
-                let cur_j = self.j;
-                self.j += 1;
-                return Some((self.i, cur_j));
-            }
-            self.i += 1;
-            self.j = self.i + 1;
-        }
-        None
-    }
-}
-
-impl ExactSizeIterator for IndexIterator {
-    fn len(&self) -> usize {
-        let mut r = 0;
-        let mut j = self.j;
-        for i in self.i..(self.len - 1) {
-            r += self.len - j;
-            j = i + 2;
-        }
-        r
     }
 }
